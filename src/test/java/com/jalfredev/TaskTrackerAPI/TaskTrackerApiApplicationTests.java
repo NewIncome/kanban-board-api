0bc277da2bf67702066a014873ba4698cb96e563
Alfredo C <alfredo_cardenas@live.com>
2026-01-16 17:14:02 -0600

    feat: add base startup maven project files - with added Spring Web dependency

---
+++ src/test/java/com/jalfredev/TaskTrackerAPI/TaskTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jalfredev.TaskTrackerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
