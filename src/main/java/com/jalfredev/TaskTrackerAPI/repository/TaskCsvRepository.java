378b3de61e6b50a94eae364ed25e69b623df9828
Alfredo C <alfredo_cardenas@live.com>
2026-01-16 19:31:13 -0600

    fea: add the Task repository - to be in charge of the data persistence to the local CSV

---
+++ src/main/java/com/jalfredev/TaskTrackerAPI/repository/TaskCsvRepository.java
@@ -0,0 +1,46 @@
+package com.jalfredev.TaskTrackerAPI.repository;
+
+import com.jalfredev.TaskTrackerAPI.domain.TaskDto;
+import com.jalfredev.TaskTrackerAPI.mapper.TaskMapper;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Repository
+public class TaskCsvRepository {
+
+  private static final String FILE_PATH = "...";
+
+  private final TaskMapper taskMapper;
+
+  public TaskCsvRepository(TaskMapper taskMapper) {
+    this.taskMapper = taskMapper;
+  }
+
+  public List<TaskDto> findAll() throws IOException {
+    try (Stream<String> lines = Files.lines(Paths.get(FILE_PATH))) {
+      return lines
+              .skip(1)  //skip header
+              .filter(line -> !line.isBlank())  //in case there's a space before
+              .map(taskMapper::toDto)
+              .collect(Collectors.toList());
+    }
+  }
+
+  public void save(TaskDto taskDto) throws IOException {
+    String row = taskMapper.fromDto(taskDto);
+
+    Files.write(
+        Paths.get(FILE_PATH),
+        row.getBytes(),
+        StandardOpenOption.APPEND
+    );
+  }
+
+}
