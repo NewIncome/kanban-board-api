306b3b28f38945263e6a91ce680d831b07e6b180
Alfredo C <alfredo_cardenas@live.com>
2026-01-20 18:55:17 -0600

    feat: add TaskControllerIntegrationTests, with test fot createTask EndPoint - remove TodoBoard code-created dir, and add it to gitignore file

---
+++ src/test/java/com/jalfredev/TaskTrackerAPI/controller/TaskControllerIntegrationTests.java
@@ -0,0 +1,57 @@
+package com.jalfredev.TaskTrackerAPI.controller;
+
+import com.jalfredev.TaskTrackerAPI.domain.Column;
+import com.jalfredev.TaskTrackerAPI.domain.TaskDto;
+import com.jalfredev.TaskTrackerAPI.service.TaskService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import tools.jackson.databind.ObjectMapper;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TaskControllerIntegrationTests {
+
+  private final MockMvc mockMvc;
+
+  private final TaskService taskService;
+
+  private final ObjectMapper objectMapper;
+
+  @Autowired
+  public TaskControllerIntegrationTests(
+      MockMvc mockMvc, TaskService taskService, ObjectMapper objectMapper) {
+    this.mockMvc = mockMvc;
+    this.taskService = taskService;
+    this.objectMapper = objectMapper;
+  }
+
+
+  @Test
+  public void testThatCreateTaskCorrectlyCreatesATaskWithStatusCode201() throws Exception {
+    TaskDto taskDto = new TaskDto(null,"My first Task", Column.TO_DO);
+    String jsonTaskDto = objectMapper.writeValueAsString(taskDto);
+
+    mockMvc.perform(
+        MockMvcRequestBuilders
+            .post("/api/tasks")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(jsonTaskDto)
+    ).andExpect(
+        MockMvcResultMatchers.jsonPath("$.id").isString()
+    ).andExpect(
+        MockMvcResultMatchers.jsonPath("$.id").isNotEmpty()
+    ).andExpect(
+        MockMvcResultMatchers.jsonPath("$.content").value("My first Task")
+    ).andExpect(
+        MockMvcResultMatchers.jsonPath("$.column").value("TO_DO")
+    );
+  }
+
+}
